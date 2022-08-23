aad3dacb1c2ef414a53643c4a1dbba3a0feaf1a9
AshKiller47 <ayaskanta11@gmail.com>
2022-08-24 02:12:23 +0530

    Initial commit

---
+++ examples/Execution.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef BOLTZMAX_EXECUTION_HPP
+#define BOLTZMAX_EXECUTION_HPP
+
+//#define EXECUTE_COLLISION
+//#define EXECUTE_SOFT_BODY
+#define EXECUTE_STAR_MERGER
+
+#endif
---
+++ src/BoltzMax/Physics/Vector2.cpp
@@ -0,0 +1,9 @@
+#include <BoltzMax/Physics/Vector2.hpp>
+
+Vector2 Vector2::operator+(const Vector2 &vector) {
+	
+	float x = speed * std::cos(angle) + vector.speed * std::cos(vector.angle), y = speed * std::sin(angle) + vector.speed * std::sin(vector.angle); return Vector2(std::hypot(x, y), std::atan2(y, x));
+	
+}
+
+Vector2 Vector2::operator*(float scalar) {	return Vector2(scalar * speed, angle);	}
